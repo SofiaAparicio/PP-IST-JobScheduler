b379104ae44793c07a141c41760742b46580ed78
Bruno Alexandre Pires Henriques <ist172913@linbase.rnl.tecnico.ulisboa.pt>
2015-04-27 12:28:02 +0100

    First Commit

---
+++ GXXX.lisp
@@ -0,0 +1,78 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Procura e Planeamento 2014/2015 - G015
+;
+; 72913 - Bruno Alexandre Pires Henriques
+; 72960 - Tiago Manuel Ferrão dos Santos
+; 
+; JobScheduler
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(in-package :user)
+
+
+;Name: calendarização
+;Arguments: External representation of the problem, search strategy:
+; - "melhor.abordagem"
+; - "a*.melhor.heuristica"
+; - "a*.melhor.heuristica.alternativa"
+; - "sondagem.iterativa"
+; - "ILDS"
+; - "abordagem.alternativa"
+;Return: The external representation of the solution
+;Side-effects: None
+
+
+(defun calendarização ())
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SEARCH STRATEGIES  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;Name: iterative-pool (sondagem iterativa
+;Arguments: ---
+;Return: ---
+;Side-effects: None
+
+(defun iterative-pool ())
+
+;Name: improved-limited-discrepancy-search (estratégia de discrepância melhorada ILDS)
+;Arguments: ---
+;Return: ---
+;Side-effects: None
+
+(defun improved-limited-discrepancy-search ())
+
+;Name: EXTRA STRATEGY TO BE DEFINED LATER
+;Arguments: ---
+;Return: ---
+;Side-effects: None
+
+(defun extra-strategy ())
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;      HEURISTICS    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Name: heuristic-1
+;Arguments: ---
+;Return: ---
+;Side-effects: None
+
+(defun heuristic-1 ())
+
+;Name: heuristic-2
+;Arguments: ---
+;Return: ---
+;Side-effects: None
+
+(defun heuristic-2 ())
